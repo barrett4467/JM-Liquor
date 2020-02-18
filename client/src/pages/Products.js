bf23be5d66f17cae5f5cd4597f27e31039ccbb30
barrett4467 <barrett4467@gmail.com>
2020-02-18 11:05:13 -0700

    Post and view routes work for items

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -3,7 +3,11 @@ import API from "../utils/API";
 
 class Products extends Component {
   state = {
-    products: []
+    products: [],
+    name: "",
+    type: "",
+    description: "",
+    price: 0
   };
 
   componentDidMount() {
@@ -13,31 +17,81 @@ class Products extends Component {
   loadItems = () => {
     API.getItems()
     .then(res => 
-      // console.log(res.data)
       this.setState({ products: res.data })
       )
     .then(console.log(this.state))
       .catch(err => console.log(err)); 
   };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.name && this.state.price && this.state.type && this.state.description) {
+      API.saveItem({
+        name: this.state.name,
+        type: this.state.type,
+        description: this.state.description,
+        price: this.state.price
+      })
+        .then(res => this.loadItems())
+        .catch(err => console.log(err));
+    }
+  };
 
   render() {
-    if (this.state.products.length){
-      return(
-        this.state.products.map(product => (
-          <>
-            <p>{product.name}</p>
-            <p>{product.type}</p>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-          </>
-        ))
-      )
-    } else {
-      return (
-        <p>We're sorry there doesn't appear to be any items available at the moment.</p>
-        );
-    }
-
+    return(
+      <>
+        <form>
+        <input
+          value={this.state.name}
+          onChange={this.handleInputChange}
+          name="name"
+          placeholder="Name (required)"
+        />
+        <input
+          value={this.state.type}
+          onChange={this.handleInputChange}
+          name="type"
+          placeholder="Type (required)"
+        />
+        <input
+          value={this.state.description}
+          onChange={this.handleInputChange}
+          name="description"
+          placeholder="Description (Required)"
+        />
+        <input
+          value={this.state.price}
+          onChange={this.handleInputChange}
+          name="price"
+          placeholder="Price (required)"
+        />
+        <button
+          disabled={!(this.state.name && this.state.price && this.state.type && this.state.description)}
+          onClick={this.handleFormSubmit}
+        >
+          Submit Book
+        </button>
+      </form>
+        {this.state.products.length ? (
+          this.state.products.map(product => (
+            <>
+              <p>{product.name}</p>
+              <p>{product.type}</p>
+              <p>{product.description}</p>
+              <p>${product.price}</p>
+            </>
+          ))
+          ) : (
+            <p>We're sorry there doesn't appear to be any items available at the moment.</p>
+          )
+        }
+      </>
+    )
   };
 
 }
